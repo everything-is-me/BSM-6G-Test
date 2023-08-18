f2a205e0c7b1d3941cb255b52f2d62c8de73c5c4
David Cuellar <david.cuellar@javeriana.edu.co>
2023-08-18 04:38:01 +0100

    feat: interoperability and fix: scalability

--- frontend/src/DataDisplay.js
+++ frontend/src/DataDisplay.js
@@ -23,6 +23,7 @@ const DataDisplay = () => {
             height: "480px",
           }}
           src="https://charts.mongodb.com/charts-blockchain-dsm-oqlvh/embed/charts?id=64d16c63-1eba-42b8-81a4-a45d6f33a5bb&maxDataAge=30&theme=light&autoRefresh=true"
+          title="Spectrum availability"
         />
         <iframe
           style={{
@@ -34,6 +35,7 @@ const DataDisplay = () => {
             height: "480px",
           }}
           src="https://charts.mongodb.com/charts-blockchain-dsm-oqlvh/embed/charts?id=9619be70-7eb5-4b95-8ccb-9e7f87614a04&maxDataAge=30&theme=light&autoRefresh=true"
+          title="Pie chart"
         />
         <iframe
           style={{
@@ -45,6 +47,7 @@ const DataDisplay = () => {
             height: "280px",
           }}
           src="https://charts.mongodb.com/charts-blockchain-dsm-oqlvh/embed/charts?id=c8cab7c2-89e2-4098-88be-dbdd7a00e4a3&maxDataAge=30&theme=light&autoRefresh=true"
+          title="Regulations rules"
         />
       </div>
     </div>
