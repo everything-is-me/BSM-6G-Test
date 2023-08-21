107e81be83bd5cc7c9307e72027ee3ee1803a5e7
David Cuellar <david.cuellar@javeriana.edu.co>
2023-08-21 04:15:52 +0100

    fix:: frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import DataDisplay from "./DataDisplay";
 function App() {
   return (
     <div className="App">
-      <DataDisplay />
       <h4 style={{ marginTop: 40, marginBottom: 5 }}>Bournemouth University</h4>
       <h5 style={{ marginTop: 0, marginBottom: 5 }}>
         Individual Master Project
@@ -16,6 +15,7 @@ function App() {
         David Felipe Cuellar Diaz
       </h5>
       <h5 style={{ marginTop: 0, marginBottom: 5 }}>August 2023</h5>
+      <DataDisplay />
     </div>
   );
 }
